7b2b69bff0ee7e4b7ecd4f54a9d628997cfd1702
Antonio Negro <anegro.floss@gmail.com>
2017-08-17 18:38:49 +0200

    Update helper scripts for images

--- img/original/get-dims.sh
+++ img/original/get-dims.sh
@@ -4,6 +4,12 @@
 #RES=100
 RES=125
 
+
+# Pasar como parámetro opcional la resolución
+if [ $# = 1 ]; then
+	RES=$1
+fi
+
 for f in *.png
 do
 	DIMS=`identify $f | cut -d" " -f3`
@@ -11,7 +17,8 @@ do
 	w=`echo $DIMS | cut -d"x" -f1`
 	h=`echo $DIMS | cut -d"x" -f2`
 	
-	c=$[ 254 * $w / ( $RES * 100 ) ]
+	#c=$[ 254 * $w / ( $RES * 100 ) ]
+	c=$( echo "scale=1; 254 * $w / ( $RES * 100 )" | bc -l )
 	
 	echo -e "$f\t$c"
 done
