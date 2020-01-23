fb4b2c6d3a5318066eb3da888390a5fc9841b37d
anegro <anegro.floss@gmail.com>
2020-01-23 18:24:22 +0100

    Use simple temp file instead of FIFO (which does not work with Dropbox)

--- utils/md2pdf.sh
+++ utils/md2pdf.sh
@@ -8,10 +8,8 @@ fi
 INPUT=$1
 OUTPUT=$2
 STYLE=$3
-FIFO=./md2pdf
+TMP_FILE=./md2pdf
 MDPY="markdown_py -v -e utf-8 -x markdown.extensions.admonition -x markdown.extensions.tables -x markdown.extensions.toc -x markdown.extensions.codehilite -x markdown.extensions.fenced_code -x markdown.extensions.attr_list -x markdown.extensions.abbr -x markdown.extensions.footnotes"
-
-mkfifo $FIFO
 
 (cat << EOF
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
@@ -25,9 +23,9 @@ EOF
 
 $MDPY $INPUT
 
-echo -e "\n</body>\n</html>") > $FIFO &
+echo -e "\n</body>\n</html>") > $TMP_FILE
 
-weasyprint -s $STYLE -f pdf $FIFO $OUTPUT
+weasyprint -s $STYLE -f pdf $TMP_FILE $OUTPUT
 
-rm $FIFO
+rm $TMP_FILE
 
